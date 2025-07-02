23b0f30f22186f24d10fc9fa3e91d461a7e565db
Elena <rahimova.elena@gmail.com>
2025-07-02 21:47:14 +0200

    feat: add apollo wrapper to layout to make it global

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import ApolloWrapper from '../components/ApolloWrapper';
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ApolloWrapper>
+          {children}
+        </ApolloWrapper>
+      </body>
+    </html>
+  );
+}
